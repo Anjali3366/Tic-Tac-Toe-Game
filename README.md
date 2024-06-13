275a162726422c3dbd3c2e2f0bfa942ca065e9c8
Anjali Tomar <160884550+Anjali3366@users.noreply.github.com>
2024-06-13 18:54:57 +0530

    First commit for initalization of files

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tic-Tac-Toe Game
+
+The Tic Tac Toe Game is a simple yet classic two-player game implemented in HTML , CSS and JavaScript . The game is played on a 3x3 grid where two players take turns marking the spaces in the grid with their respective symbols (typically "X" and "O"). The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. If all nine squares are filled and neither player has three marks in a row, the game is considered a draw.
+
+Features
+Two-player mode: Play with a friend in local multiplayer mode.
+
+Simple UI: Intuitive and easy-to-use interface.
+
+Win detection: Automatically detects and announces the winner or a draw.
+
+Restart option: Easily restart the game without exiting.
+
+
+Gameplay Instructions
+The game starts with an empty 3x3 grid.
+
+Players take turns to place their mark in an empty cell by clicking.
+
+The game checks for a win or draw after each move.
+
+The first player to get three of their marks in a row (horizontally, vertically, or diagonally) wins the game.
+
+If all cells are filled without any player getting three marks in a row, the game ends in a draw.
+
+Players can restart the game at any time.
+
+
+Created by - Anjali Tomar
---
+++ script.js
@@ -0,0 +1,85 @@
+// accessing the boxes and reset btn
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+
+// to access the turn of user
+let turnO = true;
+
+// we have to store the winning pattern to consider winner  (in array )
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+// function ::
+
+// to check the winner
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText,
+      pos2Val = boxes[pattern[1]].innerText,
+      pos3val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3val != "") {
+      if (pos1Val == pos2Val && pos2Val == pos3val) {
+        alert(`!! Congratulations , Winner is ${pos1Val} player  !!`);
+        disabledBoxes();
+        resetGame();
+      }
+    }
+  }
+};
+// to reset the game
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+};
+// to disabled all the boxes so no-one enter the X and O
+const disabledBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+// to enable all the boxes so anyone enter the X and O
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+// adding eventlistener::
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerHTML = "O";
+      turnO = false;
+    } else {
+      box.innerHTML = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    // this function will check the winner
+    checkWinner();
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  alert(`Click OK !! To reset the game `);
+  resetGame();
+});
+
+newBtn.addEventListener("click", () => {
+  alert(`Click OK !! To start the game `);
+  resetGame();
+});
